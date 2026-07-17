f692c4b6563ed00165f06a4347a10978d2fc7428
remystephanie <stephanie.remy01@proton.me>
2026-07-17 19:31:32 +0200

    Use wine region label

--- src/app/shared/components/molecules/wine-card/wine-card.component.ts
+++ src/app/shared/components/molecules/wine-card/wine-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { Bottle } from '../../../models/bottle.model';
 import { WineTypeBadgeComponent } from '../../atoms/wine-type-badge/wine-type-badge.component';
 import { WineGlassIconComponent } from '../../atoms/wine-glass/wine-glass-icon.component';
+import { WINE_REGION_LABELS, WineRegion } from '../../../models/wine-region.model';
 
 @Component({
   selector: 'app-wine-card',
@@ -13,4 +14,9 @@ import { WineGlassIconComponent } from '../../atoms/wine-glass/wine-glass-icon.c
 export class WineCardComponent {
   bottle = input.required<Bottle>();
   clicked = output<Bottle>();
+
+  regionLabel = computed(() => {
+    const region = this.bottle().region;
+    return region ? (WINE_REGION_LABELS[region as WineRegion] ?? region) : undefined;
+  });
 }
